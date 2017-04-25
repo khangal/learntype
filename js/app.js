a4464df4f9a321bc2e0c913c19018baee1febebc
Khangal <khangaljargal@yahoo.com>
2017-04-26 04:21:57 +0900

    Simple ending scenario

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 $(function(){
   window.onkeypress = function(e) {
-    console.log(e.which)
     letter = String.fromCharCode(e.which);
     screen.update(letter)
   }
@@ -14,7 +13,6 @@ var screen = {
   init: function(){
     this.cacheDom()
     this.setText("Монгол орны байгаль газарзүйн шинж төрх эрт цаг бусад улс орны адилаар олонтоо хувьсан өөрчилөгдөж байжээ.Эрин, галав солигдох бүрд нэг бол сэрүүсч эсвэл бүр дулаарах, шинэ амьтан, ургамал үүсэх эсхүл мөхөх, шинээр уул нуруу үүсэх, үгүй бол тектоник хөдөлгөөнөөр газрын гадарга доош суух үйл явц тасралтгүй явагдаж байсан нь палеонтологи, геологийн судалгаагаар тогтоогддог.Жишээлбэл одоогийн Төв Оросын нутаг буюу Москвагаас Санкт-Петербург хүртэлхи нутаг элсэн цөл байсан бол Антарктид тив халуун дулаан чийглэг уур амьсгалтай, төрөл бүрийн ургамал, амьтнаар баялаг байжээ.")
-    this.render();
   },
   cacheDom: function(){
     this.$pre = $("#pre")
@@ -35,13 +33,25 @@ var screen = {
 
   setText: function(text){
     this.text = text;
+    this.render()
   },
 
   render: function(){
+    if (this.text.length == this.cursorPosition) {
+      this.end()
+      return
+    }
+
     var pre = this.text.substring(0, this.cursorPosition)
     var post = this.text.slice(this.cursorPosition)
 
     this.$pre.text(pre)
     this.$post.text(post)
+  },
+
+  end: function() {
+    alert("ended")
+    this.cursorPosition = 0
+    this.render()
   }
 }
